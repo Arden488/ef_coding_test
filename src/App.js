ca5a508644d26b2eabf829f92dd16c25ea38d325
Anton Samoilov <absamoilov88@gmail.com>
2021-08-02 12:22:52 +0300

    Show home screen and pass settings

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
 import Header from "./components/Header/Header";
+import Home from "./components/Home/Home";
 import Quiz from "./components/Quiz/Quiz";
 import Results from "./components/Results/Results";
 import Footer from "./components/Footer/Footer";
@@ -8,9 +9,18 @@ import Footer from "./components/Footer/Footer";
 import "./App.css";
 
 function App() {
-  const [isQuizFinished, setIsQuizFinished] = useState(false);
+  const [quizStatus, setQuizStatus] = useState("NOT_STARTED");
   const [questionIndex, setQuestionIndex] = useState(0);
   const [results, setResults] = useState([]);
+  const [settings, setSettings] = useState({
+    num: 10,
+    category: 32,
+  });
+
+  const handleQuizStart = (settings) => {
+    setSettings(settings);
+    setQuizStatus("STARTED");
+  };
 
   const handleResults = (result) => {
     setResults([...results, result]);
@@ -18,21 +28,23 @@ function App() {
   };
 
   const handleQuizFinished = () => {
-    setIsQuizFinished(true);
+    setQuizStatus("FINISHED");
   };
 
   return (
     <div className="app">
       <Header />
       <div className="app-main">
-        {!isQuizFinished && (
+        {quizStatus === "NOT_STARTED" && <Home handleStart={handleQuizStart} />}
+        {quizStatus === "STARTED" && (
           <Quiz
+            settings={settings}
             questionIndex={questionIndex}
             handleFinished={handleQuizFinished}
             handleNext={handleResults}
           />
         )}
-        {isQuizFinished && <Results results={results} />}
+        {quizStatus === "FINISHED" && <Results results={results} />}
       </div>
       <Footer />
     </div>
