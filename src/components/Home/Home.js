c4aafc72315ae4ca68b43545aabbc9926aabd925
Anton Samoilov <absamoilov88@gmail.com>
2021-08-02 12:22:38 +0300

    Add home screen component

---
+++ src/components/Home/Home.js
@@ -0,0 +1,67 @@
+import { useState } from "react";
+import categories from "../../categories";
+
+import Button from "../Button/Button";
+import Select from "../Select/Select";
+import TextInput from "../TextInput/TextInput";
+import FormControl from "../FormControl/FormControl";
+
+import "./Home.css";
+
+export default function Home({ handleStart }) {
+  const [numQuestions, setNumQuestions] = useState(10);
+  const [category, setCategory] = useState(32);
+  const [numberError, setNumberError] = useState(null);
+
+  const handleNumberChange = (e) => {
+    setNumberError(null);
+
+    let value = e.target.value;
+    if (isNaN(value) || !value || value < 1 || value > 99) {
+      setNumberError("Number of questions must be between 1 and 99");
+    }
+
+    setNumQuestions(value);
+  };
+
+  const handleCategoryChange = (e) => {
+    setCategory(e.target.value);
+  };
+
+  const onStart = () => {
+    handleStart({
+      num: numQuestions,
+      category,
+    });
+  };
+
+  return (
+    <div className="home-screen">
+      <div className="home-screen-controls">
+        <FormControl label="Number of questions" error={numberError}>
+          <TextInput
+            name="num"
+            type="number"
+            size="3"
+            value={numQuestions}
+            onChange={handleNumberChange}
+          />
+        </FormControl>
+        <FormControl label="Select category">
+          <Select
+            name="category"
+            value={category}
+            options={categories}
+            onChange={handleCategoryChange}
+          />
+        </FormControl>
+      </div>
+      <p className="home-screen-intro">
+        Click "Start" button to begin a new quiz
+      </p>
+      <Button variant="large" disabled={numberError} onClick={onStart}>
+        Start
+      </Button>
+    </div>
+  );
+}
