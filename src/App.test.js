9e0d0aa64547e5f9963b832bbc7416f7fee8cae1
Anton Samoilov <absamoilov88@gmail.com>
2021-08-08 13:25:48 +0300

    Update paths in tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,6 @@
 import { act, fireEvent, render, screen } from "@testing-library/react";
 import App from "./App";
-import { AppProvider } from "./context";
+import { AppProvider } from "./AppContext";
 
 beforeEach(() => {
   fetch.resetMocks();
--- src/components/Home/Home.test.js
+++ src/components/Home/Home.test.js
@@ -1,6 +1,6 @@
 import { fireEvent, render, screen } from "@testing-library/react";
 import { useReducer } from "react";
-import { AppContext, initialState } from "../../context";
+import { AppContext, initialState } from "../../AppContext";
 import reducer from "../../reducer";
 import Home from "./Home";
 
